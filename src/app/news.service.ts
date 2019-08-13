1ff720d25cb5ed955bbda4779fb74694e2c8513b
Ottavio Monzione <ottavio.monzione@finastra.com>
2019-08-13 17:24:52 +0200

    Package.lock removed and reference to the APIs moved

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -6,13 +6,13 @@ import { HttpClient } from "@angular/common/http";
 
 import { Observable } from "rxjs";
 
-const newsURI = "http://localhost:8080/articles";
+const newsURI = "http://40.89.173.66:8080/articles";
 
 @Injectable({
   providedIn: "root"
 })
 export class NewsService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   fetchNews(): Observable<News[]> {
     return this.http.get<News[]>(newsURI);
